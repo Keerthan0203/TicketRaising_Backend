11a65adfaf4ae5595b07ef9fa1862526279d49d2
Keerthan.Rudraradhya <Keerthan.Rudraradhya@KRUDRARADHYA01>
2023-12-05 11:47:08 +0530

    FInal push

--- TicketRaising/Dto/GetAllTicketsDto.cs
+++ TicketRaising/Dto/GetAllTicketsDto.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int? EmployeeId { get; set; }
-        public int TicketTypeId { get; set; }
-        public int StatusId { get; set; }
+        public string TicketTypeId { get; set; }
+        public string StatusId { get; set; }
         public string? Description { get; set; }
         public string? CreatedBy { get; set; }
         public string? AssignedTo { get; set; }
